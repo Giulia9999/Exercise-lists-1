798652229bc148e18bd4233027149393408fb010
Giulia <giuliac35@gmail.com>
2023-02-01 12:29:36 +0100

    Create new empty array list

--- src/Main.java
+++ src/Main.java
@@ -5,19 +5,20 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args){
-        List<String> unmutableList = appendOne(Arrays.asList("Catania", "Roma", "Bari"), "Milano");
+        List<String> immutableList = appendOne(Arrays.asList("Catania", "Roma", "Bari"), "Milano");
         try {
-            unmutableList.set(1, "Tegucigalpa");
+            immutableList.set(1, "Tegucigalpa");
         }catch (Exception e){
-            System.out.println("Cannot change item of an unmutable list");
+            System.out.println("Cannot change item of an immutable list");
         }
+        System.out.println(immutableList);
 
-        System.out.println(unmutableList);
+        List <String> kingsOfRome = new ArrayList<>();
     }
 
     public static List<String> appendOne(List<String> listCity, String newCity){
-        List<String> unmutableList = new ArrayList<>(listCity);
-        unmutableList.add(newCity);
-        return Collections.unmodifiableList(unmutableList);
+        List<String> immutableList = new ArrayList<>(listCity);
+        immutableList.add(newCity);
+        return Collections.unmodifiableList(immutableList);
     }
 }
