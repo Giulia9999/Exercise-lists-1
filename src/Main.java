20afd6236c38cfd6fa2b3e272236cb9481be4e1e
Giulia <giuliac35@gmail.com>
2023-02-01 12:25:53 +0100

    Add try and catch statement, try to change element of unmutable list

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,13 @@ import java.util.List;
 public class Main {
     public static void main(String[] args){
         List<String> unmutableList = appendOne(Arrays.asList("Catania", "Roma", "Bari"), "Milano");
-        System.out.println(unmutableList);
+        try {
+            unmutableList.set(1, "Tegucigalpa");
+        }catch (Exception e){
+            System.out.println("Cannot change item of an unmutable list");
+        }
 
+        System.out.println(unmutableList);
     }
 
     public static List<String> appendOne(List<String> listCity, String newCity){
