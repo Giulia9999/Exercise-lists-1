f18df3d2f58b87f852fecc8bb796f932c289dc7e
Giulia <giuliac35@gmail.com>
2023-02-01 11:28:43 +0100

    Made non mutable list

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args){
-
+        List<String> cityList = new ArrayList<>(Arrays.asList("Roma", "Bari", "Catania"));
+        List<String> unmodifiableList = Collections.unmodifiableList(cityList);
+        unmodifiableList.add("Milano");
     }
+
 }
